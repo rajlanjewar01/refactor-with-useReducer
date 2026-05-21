36ebd126befdc880c344a77b7f88aae7c1fb7088
rajlanjewar01 <rajl.perficient@gmail.com>
2026-05-21 13:52:06 +0530

    adding useReucer skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const reducer = (state, action) => {
+
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    valueToAdd: 0
+  });
 
   const increment = () => {
-    setCount(count + 1);
+   
   };
 
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
-    setValueToAdd(value);
+    
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    
   }
 
   return (
