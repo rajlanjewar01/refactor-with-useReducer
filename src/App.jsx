66a126532afe0e04de9f2723e5799a4073f531ba
lanjewarraj <rajlanjewar98@gmail.com>
2026-05-24 21:24:50 +0530

    refactor to switch statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 import { useReducer } from 'react';
+const INCREMENT = 'increment';
+const DECREMENT = 'decrement';
+const CHANGE_VALUE_TO_ADD = 'change-value-to-add';
+const SET_VALUE_TO_ADD = 'set-value-to-add';
 
 const reducer = (state, action) => {
+  switch(action.type) {
+    case INCREMENT: 
+      return {
+        ...state,
+        count: state.count + 1
+      }
 
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1
+      }
+
+    case CHANGE_VALUE_TO_ADD:
+      return {
+        ...state,
+        valueToAdd: action.payload
+      }
+    
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0
+      }
+
+    default:
+      return state
+  }
 };
 
 function App() {
@@ -11,26 +43,43 @@ function App() {
   });
 
   const increment = () => {
-   
+   dispatch({
+    type: INCREMENT
+   });
   };
+
+  const decrement = () => {
+    dispatch({
+      type: DECREMENT
+    })
+  }
 
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
-    
+
+    dispatch({
+      type: CHANGE_VALUE_TO_ADD,
+      payload: value
+    });    
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    
+    dispatch({
+      type: SET_VALUE_TO_ADD
+    })
+
   }
+  console.log(state)
 
   return (
     <>
-      {count}
+      {state.count}
       <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>decrement</button>
 
       <form onSubmit={handleSubmit}>
-        Add a lot: <input type="number" value={valueToAdd || ''} onChange={handleChange} /> <br />
+        Add a lot: <input type="number" value={state.valueToAdd || ''} onChange={handleChange} /> <br />
         <button type="submit">Add</button>
       </form>
     </>
